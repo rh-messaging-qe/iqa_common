b0c37d5c1f5c718e406da50e355db939c399a623
Dominik Lenoch <dlenoch@redhat.com>
2017-11-22 17:02:32 +0100

    UPD(components) Artemis imports, description

--- components/brokers/artemis/artemis.py
+++ components/brokers/artemis/artemis.py
@@ -1,12 +1,12 @@
 from amom.broker import Broker
-from components.nodes import Node
+from ...nodes import Node
 
 import components.network.aplication.messaging as protocols
 
 
 class Artemis(Broker):
     """
-
+    Apache ActiveMQ Artemis has a proven non blocking architecture. It delivers outstanding performance.
     """
     supported_protocols = [protocols.Amqp10(), protocols.Mqtt(), protocols.Stomp(), protocols.Openwire()]
     name = 'Artemis'
