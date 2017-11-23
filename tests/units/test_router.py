7d0bc7c73118d4918448081afb3d6944b0aa6deb
Dominik Lenoch <dlenoch@redhat.com>
2017-11-23 18:51:47 +0100

    ADD(tests) router service tests

--- tests/units/test_router.py
+++ tests/units/test_router.py
@@ -7,3 +7,15 @@ def test_isinstance(router: Router):
 
 def test_name(router: Router):
     assert router.name == 'Qpid Dispatch Router'
+
+
+def test_restart(router: Router):
+    assert router.service._restart().get_ecode() == 0
+
+
+def test_stop(router: Router):
+    assert router.service._stop().get_ecode() == 0
+
+
+def test_start(router: Router):
+    assert router.service._start().get_ecode() == 0
