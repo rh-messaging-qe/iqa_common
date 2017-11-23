cb8e05827ce352f7fad36ce6a2c5f2e454438f8a
Dominik Lenoch <dlenoch@redhat.com>
2017-11-23 17:15:43 +0100

    UPD(amom) router docstring, name

--- amom/router/router.py
+++ amom/router/router.py
@@ -3,8 +3,12 @@ from inspect import stack
 from ..node import Node
 
 
-
 class Router:
+    """
+    Abstract messaging Router
+    """
+    name = ''
+
     def __init__(self, node: Node):
         self.node = node
         pass
