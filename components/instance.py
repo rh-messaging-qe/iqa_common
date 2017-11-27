d8b842ba6a65042470ffdfaff055ee8c5492a798
Dominik Lenoch <dlenoch@redhat.com>
2017-11-27 16:20:23 +0100

    ADD(components) iQA Components class
    It's needed in testsuite have it running instance with topology, nodes..

---
+++ components/instance.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+from .nodes.node import Node
+from .nodes.executions.ansible import AnsibleCMD
+
+
+class IQAInstance:
+    """
+    iQA helper class
+
+    Store variables, nodes and related things
+    """
+    def __init__(self, inventory=''):
+        self.inventory = inventory
+        self.nodes = []
+        self.ansible = AnsibleCMD(inventory)
+
+    def new_node(self, hostname):
+        """
+        Create new node under iQA instance
+
+        @TODO Pass inventory by true way for Ansible
+
+        :param hostname
+        :return:
+        """
+        node = Node(hostname=hostname)
+        self.nodes.append(node)
+        return node
