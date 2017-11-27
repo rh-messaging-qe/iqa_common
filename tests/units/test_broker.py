b21bd95c165d04e310ba888c2ca13e587369fdd0
Dominik Lenoch <dlenoch@redhat.com>
2017-11-27 16:23:26 +0100

    UPD(tests) test broker node ip

--- tests/units/test_broker.py
+++ tests/units/test_broker.py
@@ -10,6 +10,11 @@ def test_node(broker: Broker):
     assert isinstance(broker.node, Node)
 
 
+def test_node_ip(broker: Broker):
+    print(broker.node.ip)
+    assert broker.node.ip
+
+
 def test_exec_on_broker_node(broker: Broker):
     exec = broker.node.execute(['ls'])
     assert True if exec.get_ecode() == 0 else False
