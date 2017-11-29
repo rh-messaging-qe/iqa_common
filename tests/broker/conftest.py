dc5bc3d5d39f646f5b31cd8e6016ecb149d91bfa
Dominik Lenoch <dlenoch@redhat.com>
2017-11-29 16:14:07 +0100

    UPD(testes) typos in broker conftests

--- tests/broker/conftest.py
+++ tests/broker/conftest.py
@@ -15,39 +15,36 @@ master1 = iqa_instance.new_node(hostname='master1')
 
 
 @pytest.fixture()
-def master1(request):
+def master1():
     """
     Map Artemis broker 1 component
     :return: Broker object
     """
 
-    if 'artemis' in request.param:
-        return Artemis(node=master1)
+    return Artemis(node=master1)
 
 
-slave1 = iqa_instance.new_node(hostname='master1')
+slave1 = iqa_instance.new_node(hostname='slave1')
 
 
 @pytest.fixture()
-def slave1(request):
+def slave1():
     """
     Map Artemis broker 2 component
     :return: Broker object
     """
 
-    if 'artemis' in request.param:
-        return Artemis(node=slave1)
+    return Artemis(node=slave1)
 
 
-slave2 = iqa_instance.new_node(hostname='master1')
+slave2 = iqa_instance.new_node(hostname='slave2')
 
 
 @pytest.fixture()
-def slave2(request):
+def slave2():
     """
     Map Artemis broker 2 component
     :return: Broker object
     """
 
-    if 'artemis' in request.param:
-        return Artemis(node=slave2)
+    return Artemis(node=slave2)
