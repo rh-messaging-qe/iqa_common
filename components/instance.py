a76fa825b718e1a5ade799861902ff4f58968c8e
Dominik Lenoch <dlenoch@redhat.com>
2018-01-08 13:42:59 +0100

    UPD(components) iqa instance components implemenatiton

--- components/instance.py
+++ components/instance.py
@@ -15,17 +15,33 @@ class IQAInstance:
     def __init__(self, inventory=''):
         self.inventory = inventory
         self.nodes = []
+        self.components = []
         self.ansible = AnsibleCMD(inventory)
 
-    def new_node(self, hostname):
+    def new_node(self, hostname, ip=None):
         """
         Create new node under iQA instance
 
         @TODO Pass inventory by true way for Ansible
 
         :param hostname
+        :param ip
         :return:
         """
-        node = Node(hostname=hostname)
+        node = Node(hostname=hostname, ip=ip, ansible=self.ansible)
         self.nodes.append(node)
         return node
+
+    def new_component(self, node: Node, component):
+        """
+        Create new node under iQA instance
+
+        @TODO Pass inventory by true way for Ansible
+
+        :param node
+        :param component
+        :return:
+        """
+        component = component(node=node)
+        self.components.append(node)
+        return component
