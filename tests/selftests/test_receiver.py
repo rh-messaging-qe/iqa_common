8212c91764c5201d75eff047ec19898b45f302cf
Jakub Stejskal <stejskinek@gmail.com>
2018-01-26 13:39:45 +0100

    NEW: Comments for client tests

--- tests/selftests/test_receiver.py
+++ tests/selftests/test_receiver.py
@@ -2,15 +2,27 @@ from amom.client import Receiver
 
 
 def test_isinstance(receiver: Receiver):
+    """
+    Check if created receiver is instance of Receiver class.
+    :param receiver:
+    """
     assert isinstance(receiver, Receiver)
 
 
 def test_name(receiver: Receiver):
+    """
+    Check if created receiver is available in the test-suite.
+    :param receiver:
+    """
     clients = ['Internal core client','NodeJS RHEA client']
     assert (receiver.name in clients) == True
 
 
 def test_attributes(receiver: Receiver):
+    """
+    Check if all internal attributes are created.
+    :param receiver:
+    """
     output = False
 
     for name, value in receiver.cli_params_transformation.items():
--- tests/selftests/test_sender.py
+++ tests/selftests/test_sender.py
@@ -4,15 +4,27 @@ from odict import odict
 
 
 def test_isinstance(sender: Sender):
+    """
+    Check if created sender is instance of Sender class.
+    :param sender:
+    """
     assert isinstance(sender, Sender)
 
 
 def test_name(sender: Sender):
+    """
+    Check if created sender is available in the test-suite.
+    :param sender:
+    """
     clients = ['Internal core client','NodeJS RHEA client']
     assert (sender.name in clients) == True
 
 
 def test_attributes(sender: Sender):
+    """
+    Check if all internal attributes are created.
+    :param sender:
+    """
     output = False
 
     for name, value in sender.cli_params_transformation.items():
