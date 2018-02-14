a151a7bc9c1472fdef8cc17a7aab0b0507a5f6d1
Jakub Stejskal <jstejska@redhat.com>
2018-02-14 07:56:50 +0100

    NEW: Python external client brokerURL option construction added

---
+++ components/clients/external/python/opt/brokerurl.py
@@ -0,0 +1,44 @@
+"""
+    # TODO jstejska: Fill package info
+"""
+
+import copy
+import reformat
+from optconstruct.types import BasicComposed
+
+
+class BrokerURLPythonProton(BasicComposed):
+    """BrokerURL option parser for Proton-Python messaging client."""
+
+    def generate(self, data, client=None):
+        """Generate option brokerURL option.
+
+        :param data: data with specified option's values
+        :type data: dict
+        :param client: client's label
+        :type client: str
+        :return: option
+        :rtype: str
+        """
+        _ = client
+
+        broker_url = data.get('broker-url', None)
+
+        if broker_url is not None:
+            broker_url = self.prefix + " " + broker_url
+            return broker_url
+
+        pattern = "%{user|%s}%{password|:%s}"
+
+        credentials = reformat.reformat(pattern, data)
+
+        if credentials:
+            data_copy = copy.deepcopy(data)
+            data_copy['credentials'] = credentials
+        else:
+            data_copy = data
+
+        pattern = self._postprocessing("%{credentials|%s@}%{host}%{port|:%s}%{address|/%s}")
+        broker_url = self.prefix + " " + reformat.reformat(pattern, data_copy)
+
+        return broker_url
