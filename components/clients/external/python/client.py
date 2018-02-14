a7d272e056f5d7e8f05acec5a71e270e71680814
Jakub Stejskal <jstejska@redhat.com>
2018-02-14 07:55:47 +0100

    NEW: Python client moved and refactored with optconstruct package

--- components/clients/external/python/client.py
+++ components/clients/external/python/client.py
@@ -1,29 +1,23 @@
+"""
+    # TODO jstejska: Package description
+"""
+
 from autologging import logged, traced
 from amom.node import Node
-from odict import odict
-
 
 import components.network.aplication.messaging as protocols
-from amom.client.client import ExternalClient
-
-
-class Timeout:
-    def __init__(self, handler):
-        self.registrated_handler = handler
-
-    def on_timer_task(self, event):
-        self.registrated_handler.timeout(event)
+from components.clients.external.externalclient import ExternalClient
 
 
 @logged
 @traced
 class Client(ExternalClient):
-    """
-    Python Proton client
-    """
+    """Python Proton client."""
+
     supported_protocols = [protocols.Amqp10()]
     name = 'Python Proton client'
     version = '1.0.1'
 
     def __init__(self, node: Node):
         ExternalClient.__init__(self, node)
+
