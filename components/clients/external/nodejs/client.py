37a613f8f06faf48ddd204842a2f6045b38620b7
Jakub Stejskal <jstejska@redhat.com>
2018-02-14 07:51:39 +0100

    NEW: NodeJS external client moved and refactored with optconstruct

--- components/clients/external/nodejs/client.py
+++ components/clients/external/nodejs/client.py
@@ -1,26 +1,19 @@
+"""
+    # TODO jstejska: Package description
+"""
+
 from autologging import logged, traced
 from components.nodes.node import Node
-from odict import odict
-
 
 import components.network.aplication.messaging as protocols
-from amom.client.client import ExternalClient
-
-
-class Timeout:
-    def __init__(self, handler):
-        self.registrated_handler = handler
-
-    def on_timer_task(self, event):
-        self.registrated_handler.timeout(event)
+from components.clients.external.externalclient import ExternalClient
 
 
 @logged
 @traced
 class Client(ExternalClient):
-    """
-    NodeJS RHEA client
-    """
+    """NodeJS RHEA client"""
+
     supported_protocols = [protocols.Amqp10()]
     name = 'NodeJS RHEA client'
     version = '1.0'
