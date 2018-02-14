198d3fdcaeb8d2788cbb3b107348e7b50358bf08
Jakub Stejskal <jstejska@redhat.com>
2018-02-14 07:54:38 +0100

    NEW: Option msg-content-map construct added

---
+++ components/clients/external/opt/msgcontentmap.py
@@ -0,0 +1,9 @@
+"""
+    # TODO jstejska: Fill package info
+"""
+
+from optconstruct.types.kwoption import KWOption
+
+
+class MsgContentMap(KWOption):
+    """Basic MsgContentMap option parser for messaging clients."""
