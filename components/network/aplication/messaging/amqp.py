3eb9b0518e19d71849617d3d730a57a829b2a7b3
Dominik Lenoch <dlenoch@redhat.com>
2018-03-14 10:56:29 +0100

    UPD(components) AMQP Protocol implementation under networking

--- components/network/aplication/messaging/amqp.py
+++ components/network/aplication/messaging/amqp.py
@@ -1,22 +1,22 @@
 from autologging import logged, traced
 from amom.protocol.protocol import Protocol
-
+from components.network.transport import TCP
 
 @logged
 @traced
 class Amqp(Protocol):
-    def __init__(self, default_port=5672):
-        super(Amqp, self).__init__(default_port)
-        self.name = "AMQP 1.0"
+    def __init__(self, transport=TCP, default_port=5672):
+        super(Amqp, self).__init__(transport, default_port)
+        self.name = "AMQP"
 
 
 class Amqp10(Amqp):
-    def __init__(self, default_port=5672):
-        super(Amqp10, self).__init__(default_port)
+    def __init__(self, transport=TCP, default_port=5672):
+        super(Amqp10, self).__init__(transport, default_port)
         self.name = "AMQP 1.0"
 
 
 class Amqp091(Amqp):
-    def __init__(self, default_port=5672):
-        super(Amqp091, self).__init__(default_port)
+    def __init__(self, transport=TCP, default_port=5672):
+        super(Amqp091, self).__init__(transport, default_port)
         self.name = "AMQP 0.9.1"
