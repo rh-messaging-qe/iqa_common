4e32627fa7105507fcec3c53fc7d023409f15480
Dominik Lenoch <dlenoch@redhat.com>
2018-03-14 13:10:39 +0100

    UPD(amom) remove unused code

--- amom/router/router.py
+++ amom/router/router.py
@@ -11,8 +11,3 @@ class Router:
 
     def __init__(self, node: Node):
         self.node = node
-        pass
-
-    @staticmethod
-    def _not_supported():
-        print("Function %s is not supported for this router." % stack()[1][3])
