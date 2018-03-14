7b066bbea24bea1150da35e411e460e1dc92bb69
Dominik Lenoch <dlenoch@redhat.com>
2018-03-14 11:23:57 +0100

    NEW(amom) Address implementation

---
+++ amom/address/__init__.py
@@ -0,0 +1,4 @@
+from .anycast import Anycast
+from .multicast import Multicast
+from .mixed import Mixed
+from .address import Address
---
+++ amom/address/address.py
@@ -0,0 +1,6 @@
+class Address:
+    """
+    Abstract address class
+    """
+    def __init__(self, value):
+        self.value = value
