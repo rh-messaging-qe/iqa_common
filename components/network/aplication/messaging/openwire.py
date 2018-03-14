b21963ba76d6e36d9ecfdee5d4d90db132306ddd
Dominik Lenoch <dlenoch@redhat.com>
2018-03-14 10:57:02 +0100

    UPD(components) OpenWire Protocol implementation under networking

--- components/network/aplication/messaging/openwire.py
+++ components/network/aplication/messaging/openwire.py
@@ -1,9 +1,10 @@
 from autologging import logged, traced
 from amom.protocol.protocol import Protocol
+from components.network.transport import TCP
 
 
 @logged
 @traced
 class Openwire(Protocol):
-    def __init__(self, default_port=5672):
-        super(Openwire, self).__init__(default_port)
+    def __init__(self, transport=TCP, default_port=5672):
+        super(Openwire, self).__init__(transport, default_port)
