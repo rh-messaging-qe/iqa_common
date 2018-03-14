89aae27ba004948e925c65d5d0e04e7628c0af81
Dominik Lenoch <dlenoch@redhat.com>
2018-03-14 10:58:01 +0100

    UPD(amom) Protocol abstraction

--- amom/protocol/protocol.py
+++ amom/protocol/protocol.py
@@ -1,5 +1,7 @@
-class Protocol(object):
-    def __init__(self, default_port=None):
+class Protocol:
+    """ Protocol abstraction"""
+    def __init__(self, transaction, transport, default_port=None):
         self.name = type(self).__name__
         self.default_port = default_port
-
+        self.transaction = transaction
+        self.transport = transport
