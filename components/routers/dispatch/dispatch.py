3ebb6f1d94c114c195886879f4fa024e254d3288
Dominik Lenoch <dlenoch@redhat.com>
2018-03-14 13:56:13 +0100

    NEW(components) qpid-dispatch version, config_refresh

--- components/routers/dispatch/dispatch.py
+++ components/routers/dispatch/dispatch.py
@@ -1,6 +1,8 @@
 from autologging import logged, traced
+
 from amom.router import Router
 from amom.node import Node
+
 from .management import QDManage, QDStat
 from ...service import Service
 
@@ -8,14 +10,39 @@ from ...service import Service
 @logged
 @traced
 class Dispatch(Router):
-    name = 'Qpid Dispatch Router'
     """
-    Qpid Dispatch component
+    Dispatch router component
     """
 
-    def __init__(self, node: Node):
+    name = 'Qpid Dispatch Router'
+
+    def __init__(self, node: Node, service='qdrouterd'):
         Router.__init__(self, node=node)
         self.qdmanage = QDManage()
         self.qdstat = QDStat()
         self.config = None
         self.service = Service(self, 'qdrouterd')
+
+        self._service = service
+        self.service = Service(node, self._service)
+        self._version = None
+
+    @staticmethod
+    def config_refresh():
+        """
+        Read and save router config from remote
+        :return:
+        """
+        pass
+
+    @property
+    def version(self):
+        """
+        Get qdrouterd version
+        :return:
+        """
+        if self._version:
+            return self._version
+        else:
+            cmd = self.node.execute(['qdrouterd', '-v'])
+            return cmd
