080ea55eeb77abc695150b1d41c20d63770979b7
Dominik Lenoch <dlenoch@redhat.com>
2018-03-14 10:56:45 +0100

    UPD(components) MQTT Protocol implementation under networking

--- components/network/aplication/messaging/mqtt.py
+++ components/network/aplication/messaging/mqtt.py
@@ -1,9 +1,9 @@
 from autologging import logged, traced
 from amom.protocol.protocol import Protocol
-
+from components.network.transport import TCP
 
 @logged
 @traced
 class Mqtt(Protocol):
-    def __init__(self, default_port=5672):
-        super(Mqtt, self).__init__(default_port)
+    def __init__(self, transport=TCP, default_port=5672):
+        super(Mqtt, self).__init__(transport, default_port)
