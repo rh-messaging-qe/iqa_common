9ad0f1189869037231a2e7d6084955dd047c181c
Dominik Lenoch <dlenoch@redhat.com>
2018-03-14 11:25:00 +0100

    NEW(amom) Adress multicast implementation

---
+++ amom/address/multicast.py
@@ -0,0 +1,12 @@
+from .address import Address
+from ..route import Multicast as MulticastRoute
+
+
+class Multicast(Address, MulticastRoute):
+    """
+    Every queue within the matching address, in a publish-subscribe manner.
+    """
+    def __init__(self, value):
+        Address.__init__(self, value)
+        MulticastRoute.__init__(self)
+
