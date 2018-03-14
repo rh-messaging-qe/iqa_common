55e1c8c31f18fee99502af673d2365c44df43729
Dominik Lenoch <dlenoch@redhat.com>
2018-03-14 11:24:34 +0100

    NEW(amom) Adress anycast implementation

---
+++ amom/address/anycast.py
@@ -0,0 +1,11 @@
+from .address import Address
+from ..route import Anycast as AnycastRoute
+
+
+class Anycast(Address, AnycastRoute):
+    """
+    A single queue within the matching address, in a point-to-point manner.
+    """
+    def __init__(self, value):
+        Address.__init__(self, value)
+        AnycastRoute.__init__(self)
