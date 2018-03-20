a0d63ebd81f6c95d57a5ec3c02f0ff351fa3c4fe
Dominik Lenoch <dlenoch@redhat.com>
2018-03-20 17:43:12 +0100

    FIX(components, amom) Clients abstraction code

--- components/clients/core/client.py
+++ components/clients/core/client.py
@@ -5,7 +5,7 @@
 from autologging import logged, traced
 
 import components.network.aplication.messaging as protocols
-from amom.client.client import NativeClient
+from amom.client.client import Client
 
 
 class Timeout:
@@ -20,13 +20,12 @@ class Timeout:
 
 @logged
 @traced
-class Client(NativeClient):
+class Client(Client):
     """Internal core Proton mapping client."""
 
     supported_protocols = [protocols.Amqp10()]
     name = 'Internal core client'
     version = '0.1'
-
 
     def __init__(self):
         super(Client, self).__init__()
