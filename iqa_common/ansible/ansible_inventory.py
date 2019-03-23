ad5cc03d4723bc0f4157d65becf370885417eb58
Michal Tóth <mtoth@redhat.com>
2019-03-23 18:51:39 +0100

    NEW(Ansible) VirtualAnsible object representation

--- iqa_common/ansible/ansible_inventory.py
+++ iqa_common/ansible/ansible_inventory.py
@@ -6,7 +6,21 @@ from ansible.template import Templar
 import logging
 
 
+class AnsibleVirtualComponent(object):
+
+    def __init__(self, name: str, type: str, implementation: str, members: list, **kwargs):
+        self.name = name
+        self.type = type
+        self.implementation = implementation
+        self.members = members
+        self.kwargs = kwargs
+        self.component = None  # real component, once created and populated with member
+
+
 class AnsibleInventory(object):
+
+    virt_component = 'virtual_component'
+
     def __init__(self, inventory: str=None, extra_vars: dict=None):
         self._logger = logging.getLogger(self.__class__.__module__)
         self.inventory = inventory
@@ -40,3 +54,23 @@ class AnsibleInventory(object):
         data = self.var_mgr.get_vars(host=host)
         templar = Templar(variables=data, loader=self.loader)
         return templar.template(data, fail_on_undefined=False)
+
+    def get_virtual_components(self) -> list:
+        virtual_components = []
+        for group in self.inv_mgr.get_groups_dict():
+            group_vars = self.inv_mgr.groups[group].get_vars()
+
+            if self.virt_component in group_vars:
+                vcmp_type = group_vars.get(self.virt_component)
+                group_vars.pop('virtual_component')
+                vcmp_impl = group_vars.get('implementation')
+                group_vars.pop('implementation')
+                hosts = self.inv_mgr.groups[group].get_hosts()
+                vcmp = AnsibleVirtualComponent(name=group,
+                                               type=vcmp_type,
+                                               implementation=vcmp_impl,
+                                               members=hosts,
+                                               **group_vars)
+
+                virtual_components.append(vcmp)
+        return virtual_components
